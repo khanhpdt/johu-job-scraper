8765acf0dba81ced8c74202e2a4396ef7ea8037b
Khanh Pham <khanhpdtvn@gmail.com>
2019-12-25 21:22:41 +0700

    Run scrapers

--- src/main/scala/vn/johu/app/AppRootActor.scala
+++ src/main/scala/vn/johu/app/AppRootActor.scala
@@ -1,17 +1,27 @@
 package vn.johu.app
 
+import scala.collection.mutable
+
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
-import akka.actor.typed.{Behavior, PostStop, Signal}
+import akka.actor.typed.{ActorRef, Behavior, PostStop, Signal}
 
 import vn.johu.messaging.RabbitMqClient
 import vn.johu.persistence.MongoDb
 import vn.johu.scheduling.QuartzScheduler
+import vn.johu.scraping.Scraper
+import vn.johu.scraping.Scraper.JobsScraped
+import vn.johu.scraping.itviec.ItViecScraper
+import vn.johu.scraping.jsoup.JSoup
 import vn.johu.utils.Logging
 
 class AppRootActor(context: ActorContext[AppRootActor.Command])
   extends AbstractBehavior[AppRootActor.Command] with Logging {
 
   import AppRootActor._
+
+  private var scrapers = mutable.ListBuffer.empty[ActorRef[Scraper.Command]]
+
+  private val jobsScrapedAdapter = context.messageAdapter[JobsScraped](WrappedJobsScraped.apply)
 
   override def onMessage(msg: Command): Behavior[Command] = {
     msg match {
@@ -20,6 +30,9 @@ class AppRootActor(context: ActorContext[AppRootActor.Command])
         this
       case RunAllScrapers =>
         runAllScrapers()
+        this
+      case WrappedJobsScraped(jobsScraped) =>
+        logger.debug(s"Scraped ${jobsScraped.scrapedJobs.size} jobs")
         this
     }
   }
@@ -43,6 +56,18 @@ class AppRootActor(context: ActorContext[AppRootActor.Command])
 
   private def runAllScrapers(): Unit = {
     logger.info("Run all scrapers")
+
+    if (scrapers.isEmpty) {
+      logger.info("Found no scrapers. Creating them...")
+      scrapers += context.spawn[Scraper.Command](ItViecScraper(JSoup), "ItViecScraper")
+      logger.info(s"Created ${scrapers.size} scrapers: ${scrapers.map(_.path).mkString(",")}")
+    }
+
+    logger.info(s"Sending messages to ${scrapers.size} scrapers to start scraping...")
+
+    scrapers.foreach { scraper =>
+      scraper ! Scraper.Scrape(replyTo = jobsScrapedAdapter)
+    }
   }
 }
 
@@ -53,6 +78,8 @@ object AppRootActor {
   case object InitSystem extends Command
 
   case object RunAllScrapers extends Command
+
+  case class WrappedJobsScraped(jobsScraped: JobsScraped) extends Command
 
   def apply(): Behavior[Command] = {
     Behaviors.setup[Command](new AppRootActor(_))
