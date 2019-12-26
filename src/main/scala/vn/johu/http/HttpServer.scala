6753f944e57e70a64c408ea9dd6344fa583c567d
Khanh Pham <khanhpdtvn@gmail.com>
2019-12-26 22:24:44 +0700

    Add log

--- src/main/scala/vn/johu/http/HttpServer.scala
+++ src/main/scala/vn/johu/http/HttpServer.scala
@@ -1,13 +1,19 @@
 package vn.johu.http
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.adapter._
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
 import akka.stream.ActorMaterializer
 
 import vn.johu.utils.Logging
 
 object HttpServer extends Logging {
+
+  private var serverBinding: ServerBinding = _
 
   def init(actorSystem: ActorSystem[Nothing]): Unit = {
     logger.info("Starting HttpServer...")
@@ -17,11 +23,16 @@ object HttpServer extends Logging {
     implicit val am: ActorMaterializer = ActorMaterializer()(untypedSystem)
 
     val routes = new Routes()
-    Http().bindAndHandle(routes.routes, "localhost", 9090)
+
+    val port = 9090
+    serverBinding = Await.result(Http().bindAndHandle(routes.routes, "localhost", port), 3.minutes)
+
+    logger.info(s"HttpServer started and is listening on port $port")
   }
 
   def close(): Unit = {
     logger.info("Closing HttpServer. TBD.")
+    serverBinding.terminate(3.minutes)
   }
 
 }
